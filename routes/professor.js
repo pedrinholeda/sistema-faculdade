d584b54033667699b7af5b1380d6dc0a39e62722
Pedro Leda <pedro.henrique.leda@gmail.com>
2019-10-28 17:41:55 -0300

    feita listagem de materias para lançamento de notas

--- routes/professor.js
+++ routes/professor.js
@@ -10,6 +10,7 @@ const Postagem = mongoose.model("postagens");
 const Curso = mongoose.model("cursos");
 const Usuario = mongoose.model("usuarios"); // em teste
 const { eProfessor } = require("../helpers/eProfessor");
+const { eAdmin } = require("../helpers/eAdmin");
 
 router.get("/", eProfessor, (req, res) => {
   res.render("professor/index");
@@ -139,6 +140,57 @@ router.post("/postagens/deletar", eProfessor, (req, res) => {
     .catch(err => {
       req.flash("error_msg", "Houve um erro ao deletar a Postagem");
       res.redirect("/professor/postagens");
+    });
+});
+
+router.get("/view-notas", eProfessor, (req, res) => {
+  Materia.find()
+    .populate("curso")
+    .sort({ data: "desc" })
+    .then(materias => {
+      res.render("professor/view-notas", { materias: materias });
+    })
+    .catch(err => {
+      req.flash("error_msg", "Houve um erro ao listar as materias");
+      res.redirect("/professor");
+    });
+});
+
+//rota view materia/edit
+router.get("/materias/notas/edit/:id", eProfessor, async (req, res) => {
+  Materia.findOne({ _id: req.params.id })
+    .then(materia => {
+      const matricula = []; //array de alunos
+      //for para colocar os alunos matriculados dentro de matricula
+      for (var i = 0; i < materia.matriculados.length; i++) {
+        matricula.push(materia.matriculados[i].user);
+      }
+
+      //global.matri = disciplina._id;
+      const discID = [];
+
+      discID.push({ text: materia._id });
+      Usuario.find({ _id: matricula })
+        .then(usuario => {
+          //return res.send({ usuario });
+          res.render("professor/notas", {
+            usuario: usuario,
+            discID: discID
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          req.flash("error_msg", "Houve error interno ao testar");
+          res.redirect("/");
+        });
+    })
+    .catch(err => {
+      console.log("err: ", err);
+      req.flash(
+        "error_msg",
+        "Houve error ao carregar o formulario de lançamento"
+      );
+      res.redirect("/professor");
     });
 });
 
